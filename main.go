a854c11ef3cf9acfe9365a8d366b2fdc802b59e6
tiyan-attirmidzi <tiyanattirmidzi20@gmail.com>
2022-07-14 00:01:13 +0700

    add .ignore fix dns db with dotenv

--- main.go
+++ main.go
@@ -1,19 +1,28 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"api.pustaka/book"
 	"api.pustaka/handlers"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func main() {
 
-	dsn := "root:18M@reT20@tcp(127.0.0.1:3306)/api_pustaka?charset=utf8mb4&parseTime=True&loc=Local"
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", os.Getenv("DB_USER"), os.Getenv("DB_PASS"), os.Getenv("DB_HOST"), os.Getenv("DB_NAME"))
+	fmt.Println(dsn)
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if err != nil {
